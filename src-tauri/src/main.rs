f88c4186bb08fd3d09ab0fe049e109ef6ceac879
Gabriel Trajano <conversarcomgabriel@gmail.com>
2026-06-12 15:44:39 -0300

    App finally with bundle

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,7 +8,8 @@ mod commands;
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_updater::Builder::new().build())
+        // Ajustado para o padrão que valida as permissões do seu arquivo capabilities JSON
+        .plugin(tauri_plugin_updater::Builder::default().build())
         .plugin(tauri_plugin_sql::Builder::default().build())
         .plugin(tauri_plugin_dialog::init())
         .invoke_handler(tauri::generate_handler![
